8ebdc7ec6802863de14a44e8773a32e064528bbc
Markov A <67405201+naugale@users.noreply.github.com>
2020-07-14 11:05:20 +0300

    14.07.2020

--- bot_for_yandex.js
+++ bot_for_yandex.js
@@ -6,13 +6,14 @@
 // @author       You
 // @match        https://yandex.ru/*
 // @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
+// @match        https://crushdrummers.ru/*
 // @grant        none
 // ==/UserScript==
 
-let keywords = ["Гобой", "Как звучит флейта", "Тромбон", "Что такое валторна", "Фагот", "Скрипка", "Виолончель"];
-let keyword = keywords[getRandom(0,keywords.length)];
+
 let searchBtn = document.getElementsByClassName('mini-suggest__button')[0];
 let links = document.links;
+let site = location.host;
 function getRandom(min,max){
   return Math.floor(Math.random()*(max-min)+min);
 }
@@ -30,30 +31,52 @@ function writhKeyword(word){
     },1000);
 }
 
-if(document.getElementsByClassName('home-logo__default')[0] !=undefined)
-   writhKeyword(keyword);
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+// Если document.getElementsByClassName('home-logo__default')[0] !=undefined, значит мы на главной странице YANDEX
+if(document.getElementsByClassName('home-logo__default')[0] !=undefined){
+    let sites ={
+        "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai":["Гобой", "Как звучит флейта", "Тромбон", "Что такое валторна", "Фагот", "Скрипка", "Виолончель"],
+        "crushdrummers.ru":["Шоу барабанщиков","Барабанное шоу","Заказать выступление барабанщиков","Crush барабанщики"]
+    };
+    let site = Object.keys(sites)[getRandom(0,Object.keys(sites).length)];
+    let keywords = sites[site];
+    let keyword = keywords[getRandom(0,keywords.length)];
+    document.cookie = "site="+site;
+    writhKeyword(keyword);
+}
+//Иначе если location.hostname == "yandex.ru", то это страница с поисковой выдачей
 else if (location.hostname =="yandex.ru"){
     let flag = true;
+    let site = getCookie("site");
     for(let i=0;i<links.length;i++){
-       if(links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai")!=-1){
-         flag=false;
+       if(links[i].href.indexOf(site)!=-1){
+           let link = links[i];
+           flag=false;
          links[i].setAttribute('target', '_self');
-         links[i].click();
+         setTimeout(()=>{link.click()},getRandom(1000,3000));
          break;
        }
 
     }
-    if (document.getElementsByClassName('pager__item pager__item_current_yes pager__item_kind_page')[0].textContent > 9){
+    if (document.getElementsByClassName('pager__item pager__item_current_yes pager__item_kind_page')[0].textContent > 10){
         flag = false;
         location.href = "https://yandex.ru/";
     }
     if (flag) setTimeout(()=>{document.getElementsByClassName('pager__item_kind_next')[0].click()},getRandom(3000,6000));
-}  else {
+}
+// Иначе мы находимся на искомом сайте
+else {
      setInterval(()=>{
          if (getRandom(0,101)>=70) location.href = "https://yandex.ru/";
          else{
              let index = getRandom(0,links.length);
-             if(links[index].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai")!=-1)
+             if(links[index].href.indexOf(site)!=-1)
                  links[index].click();
          }
      },getRandom(2000,6000));
